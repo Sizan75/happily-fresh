799eb56248b21b8fc35d161a58c1177c4e4dd804
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-07 22:29:20 +0600

    main layout, route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { route } from './Routes/Route/Route';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <RouterProvider router={route}></RouterProvider>
   );
 }
 
