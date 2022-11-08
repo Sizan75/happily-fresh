d694150992329e330c05c5dbf27be4b8d1e9d7ab
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-08 20:21:03 +0600

    log in design done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import login from '../../assets/login/signup.png'
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+    const handleGoogleSignIn = () =>{
+
+    }
+    const handleLogin = () =>{
+        
+    }
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center ">
+                    <img src={login} className='w-3/5 h-3/5 ' alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <h3 className='text-xl font-bold'>Log In</h3>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered input-info w-full max-w-xs" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered input-info w-full max-w-xs" required />
+                            <label className="label">
+                                <Link to="/signup" className="label-text-alt link link-hover">
+                                    Not registered yet? <span className=' text-orange-700'>Sign Up</span></Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button onClick={handleLogin} className="btn btn-primary">Log In</button>
+                            <button onClick={handleGoogleSignIn} className="btn btn-outline btn-primary mt-1 text-dark"><FcGoogle></FcGoogle>  Google Sign In</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <Link to="" className="label-text-alt link link-hover">Forgot password?</Link>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-        </div>
     );
 };
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -5,55 +5,56 @@ import signup from '../../assets/login/signup.png'
 
 const SignUp = () => {
 
-    const handleGoogleSignIn = () =>{
+    const handleGoogleSignIn = () => {
 
     }
     return (
         <div>
-        <div className="hero min-h-screen bg-base-200">
-<div className="hero-content flex-col lg:flex-row">
-<div className="text-center ">
-  <img src={signup} className='w-3/5 h-3/5 ' alt="" />
-</div>
-<div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-  <form className="card-body">
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text">User Name</span>
-      </label>
-      <input type="text" name='name' placeholder="user name" className="input input-bordered input-info w-full max-w-xs" />
-    </div>
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text">Image URL</span>
-      </label>
-      <input type="text" name='imageURL' placeholder="Image URL" className="input input-bordered input-info w-full max-w-xs" />
-    </div>
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text">Email</span>
-      </label>
-      <input type="email" name='email' placeholder="email" className="input input-bordered input-info w-full max-w-xs" required />
-    </div>
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text">Password</span>
-      </label>
-      <input type="password" name='password' placeholder="password" className="input input-bordered input-info w-full max-w-xs" required />
-      <label className="label">
-        <Link to="/login" className="label-text-alt link link-hover">
-            Already have an account? <span className=' text-orange-700'>Log In</span></Link>
-      </label>
-    </div>
-    <div className="form-control mt-6">
-      <button className="btn btn-primary">SignUp</button>
-      <button onClick={handleGoogleSignIn} className="btn btn-outline btn-primary mt-1 text-dark"><FcGoogle></FcGoogle>  Google Sign In</button>
-    </div>
-  </form>
-</div>
-</div>
-</div>
-    </div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="text-center ">
+                        <img src={signup} className='w-3/5 h-3/5 ' alt="" />
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <h4 className='text-xl font-bold'>Sign Up</h4>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">User Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="user name" className="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Image URL</span>
+                                </label>
+                                <input type="text" name='imageURL' placeholder="Image URL" className="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered input-info w-full max-w-xs" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered input-info w-full max-w-xs" required />
+                                <label className="label">
+                                    <Link to="/login" className="label-text-alt link link-hover">
+                                        Already have an account? <span className=' text-orange-700'>Log In</span></Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">SignUp</button>
+                                <button onClick={handleGoogleSignIn} className="btn btn-outline btn-primary mt-1 text-dark"><FcGoogle></FcGoogle>  Google Sign In</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
