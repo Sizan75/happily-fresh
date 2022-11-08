bb7c7fe32ad560f7bc35171f428fdaa7994a36f5
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-09 05:17:58 +0600

    photo view used in food image

--- src/components/Home/FoodService/FoodServiceCart.js
+++ src/components/Home/FoodService/FoodServiceCart.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
-const FoodServiceCart = ({food}) => {
-    const {image,title,price,details,_id}=food
+const FoodServiceCart = ({ food }) => {
+    const { image, title, price, details, _id } = food
     return (
         <div>
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
-               <img src={image} className="h-64" alt="food" />
+                <PhotoProvider>
+                    <PhotoView src={image} className="w-full">
+                        <img src={image} className='h-64' alt="food" />
+                    </PhotoView>
+                </PhotoProvider>
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
-                    <p>{details.slice(1,100) + '...'}</p>
+                    <p>{details.slice(1, 100) + '...'}</p>
                     <h2 className='font-semibold text-xl'>Price: ${price}</h2>
                     <div className="card-actions ">
                         <Link to={`/foodservice/${_id}`}><button className="btn btn-primary w-full">View Details</button></Link>
